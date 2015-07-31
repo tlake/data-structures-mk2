eb14f35f64f34fc2e07366fa7a69fb45219dc265
Nicholas Draper <ndraper2@gmail.com>
2015-07-31 14:21:15 -0700

    docstrings for hash table functions

--- structures/hash_table.py
+++ structures/hash_table.py
@@ -5,15 +5,22 @@ from __future__ import unicode_literals
 class HashTable(object):
 
     def __init__(self, size):
+        """Initialize a hash table with the provided size."""
         self.table_list = [[] for x in xrange(size)]
 
     def get(self, key):
+        """Return the value associated with the provided key."""
         hashed = self._hash(key)
         for item in self.table_list[hashed]:
             if key == item[0]:
                 return item[1]
 
     def set(self, key, value):
+        """Set a key value pair in the table.
+
+        Keys must be strings.
+        If the key is already in the table, updates its value.
+        """
         # updates value if key is in the list
         if not isinstance(key, basestring):
             raise TypeError('Keys must be strings.')
