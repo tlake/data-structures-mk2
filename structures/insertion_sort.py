da9126ec845422bddb914eaf767b1f759ce655c2
tlake <tanner.lake@gmail.com>
2015-08-03 13:28:22 -0700

    first pass at insertion sort

--- structures/insertion_sort.py
+++ structures/insertion_sort.py
@@ -1,2 +1,20 @@
 # -*- coding utf-8 -*-
 from __future__ import unicode_literals
+
+
+def insertion_sort(iterable):
+    sorted_list = []
+
+    sorted_list.append(iterable[0])
+
+    for i in iterable[1::]:
+        for index, s in enumerate(sorted_list):
+            if i <= s:
+                sorted_list.insert(index, i)
+                print "inserted " + str(i)
+            else:
+                sorted_list.append(i)
+                print "appended " + str(i)
+            break
+
+    return sorted_list
