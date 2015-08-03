22879b451c630b7244349382cdfe3db140f1a344
Nicholas Draper <ndraper2@gmail.com>
2015-08-03 14:27:32 -0700

    modifed sort to update in place

--- structures/insertion_sort.py
+++ structures/insertion_sort.py
@@ -3,22 +3,12 @@ from __future__ import unicode_literals
 
 
 def insertion_sort(iterable):
-    sorted_list = []
-
-    sorted_list.append(iterable[0])
-
-    for i in iterable[1::]:
-        inserted = False
-
-        if i > sorted_list[-1]:
-            sorted_list.append(i)
-            inserted = True
-
-        while not inserted:
-            for index, sorted_item in enumerate(sorted_list):
-                if i <= sorted_item:
-                    sorted_list.insert(index, i)
-                    inserted = True
-                    break
-
-    return sorted_list
+    for i in xrange(1, len(iterable)):
+        item = iterable[i]
+        j = i
+        while item < iterable[j-1] and j > 0:
+            iterable[j] = iterable[j-1]
+            j -= 1
+        iterable[j] = item
+        print iterable, item
+    return iterable
