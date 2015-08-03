42b4bf1d71c4067beab7ada46b5453b408929561
tlake <tanner.lake@gmail.com>
2015-08-03 14:06:12 -0700

    working implementation, test passing

--- structures/insertion_sort.py
+++ structures/insertion_sort.py
@@ -8,13 +8,17 @@ def insertion_sort(iterable):
     sorted_list.append(iterable[0])
 
     for i in iterable[1::]:
-        for index, s in enumerate(sorted_list):
-            if i <= s:
-                sorted_list.insert(index, i)
-                print "inserted " + str(i)
-            else:
-                sorted_list.append(i)
-                print "appended " + str(i)
-            break
+        inserted = False
+
+        if i > sorted_list[-1]:
+            sorted_list.append(i)
+            inserted = True
+
+        while not inserted:
+            for index, sorted_item in enumerate(sorted_list):
+                if i <= sorted_item:
+                    sorted_list.insert(index, i)
+                    inserted = True
+                    break
 
     return sorted_list
