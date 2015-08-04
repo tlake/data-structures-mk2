88d92d64b8630aede451d198cabe6f6d4c1e6bcc
tlake <tanner.lake@gmail.com>
2015-08-04 12:52:34 -0700

    just a skellington

---
+++ structures/merge_sort.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+# or maybe we don't?
+# ONLY TIME
+#   (and maybe Cris)
+#   (and maybe that other guy)
+# WILL TELL US
+
+
+def merge_sort(iterable):
+    pass
+
+
+def _merge(left, right):
+    pass
